8b1bb34f5211d7b23745f925f689e88e9d10a08d
NfrXDRA <74735777+NfrXDRA@users.noreply.github.com>
2020-11-20 17:21:32 +0300

    Create Eyadinstall.py

---
+++ Eyadinstall.py
@@ -0,0 +1,53 @@
+import os
+
+os.system ('pkg install openssh -y')
+
+os.system ('pkg install wget -y')
+
+os.system ('pkg install git -y')
+
+os.system ('pkg install python2 -y')
+
+os.system ('pkg install php -y')
+
+os.system ('pkg install nano -y')
+
+os.system ('pkg install python -y')
+
+os.system ('pkg install git -y')
+
+os.system ('pkg install python3 -y')
+
+os.system ('pkg install python2 -y')
+
+os.system ('pkg install git -y')
+
+os.system ('pkg install python -y')
+
+os.system ('pkg install openssh -y')
+
+os.system ('pkg install php -y')
+
+os.system ('pkg install git -y')
+
+os.system ('pkg install python -y')
+
+os.system ('pkg install python2 -y')
+
+os.system ('pkg install openssh -y')
+
+os.system ('pkg install tor -y')
+
+os.system ('pkg install uzip -y')
+
+os.system ('pkg install net-tools -y')
+
+os.system ('pkg install unrar -y')
+
+os.system ('pkg install proot -y')
+
+os.system ('install wget -y')
+
+os.system ('pip2 install requests -y')
+
+os.system ('pkg install php -y')
